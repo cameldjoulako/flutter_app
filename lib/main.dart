5c96d8e03bdae403d547e3ffaf474fa9e3c53a66
Camel Djoulako <cameldjoulako17@gmail.com>
2021-06-17 17:52:27 +0100

    list view widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,55 @@ void main() {
 }
 
 class HomePage extends StatelessWidget {
+  final euroList = [
+    'Allemagne',
+    'Autriche',
+    'Belgique',
+    'Chypre',
+    'Croatie',
+    'Danemark',
+    'Espagne',
+    'Estonie',
+    'Autriche',
+    'Belgique',
+    'Chypre',
+    'Croatie',
+    'Danemark',
+    'Espagne',
+    'Roumanie',
+    'Autriche',
+    'Belgique',
+    'Chypre',
+    'Croatie',
+    'Danemark',
+    'Espagne',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Learn Flutter with Camel Djoulako"),
+        title: Text("Learn Flutter with Homedeve.com"),
       ),
-      body: Container(),
+      body: ListView.builder(
+        itemCount: euroList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(euroList[index]),
+            leading: CircleAvatar(backgroundColor: Colors.blue),
+          );
+        },
+      ),
+
+      /* ListView(
+        children: [
+          for (var country in euroList)
+            ListTile(
+              title: Text(country),
+              subtitle: Text(country),
+              leading: CircleAvatar(backgroundColor: Colors.blue),
+            ),
+        ],
+      ), */
     );
   }
 }
